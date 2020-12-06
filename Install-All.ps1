5ca794c780f7f7f143eab9997343d5470e6ebf57
stic <1974622+stic@users.noreply.github.com>
2020-12-06 16:51:01 +0000

    Update Install-All.ps1
    Added googlechrome, pwsh, microsoft-windows-terminal, sysinternals

--- Install-All.ps1
+++ Install-All.ps1
@@ -1,12 +1,20 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 
+choco install "microsoft-windows-terminal" -y
+choco install "pwsh" -y
+
 choco install "notepadplusplus"
+choco pin add -n "notepadplusplus"
 
 choco install "vscode"
 choco pin add -n "vscode"
  
 choco install "7zip"
+choco install "sysinternals" -y
 
 choco install "dnspy" 
 
 choco install "vlc"
+
+choco install "googlechrome"
+choco pin add -n "googlechrome"

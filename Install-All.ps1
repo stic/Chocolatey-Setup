4fb386c3880f5788d6bbd9959659fb8bbad27eb7
stic <1974622+stic@users.noreply.github.com>
2020-12-06 19:08:17 +0000

    Update Install-All.ps1
    Adding game lunchers (gog, epic, steam), Docker Desktop, Adobe Reader, etc

--- Install-All.ps1
+++ Install-All.ps1
@@ -2,19 +2,31 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 choco install "microsoft-windows-terminal" -y
 choco install "pwsh" -y
+choco install "dnspy" 
 
 choco install "notepadplusplus"
 choco pin add -n "notepadplusplus"
 
 choco install "vscode"
 choco pin add -n "vscode"
- 
-choco install "7zip"
+
+choco install "docker-desktop" -y
+choco pin add -n "docker-desktop"
+
 choco install "sysinternals" -y
 
-choco install "dnspy" 
-
 choco install "vlc"
+choco install "7zip"
 
 choco install "googlechrome"
 choco pin add -n "googlechrome"
+
+choco install "adobereader" -y
+choco pin add -n "adobereader"
+
+choco install "goggalaxy" -y
+choco pin add -n "goggalaxy"
+choco install "steam" -y
+choco pin add -n "steam"
+choco install "epicgameslauncher" -y
+choco pin add -n "epicgameslauncher"
